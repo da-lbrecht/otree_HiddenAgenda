1b4bc283b08714f5b74497548ffdfd36d5bf5a60
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-16 15:17:16 +0200

    Update taskintro.mov and add payment information.

--- hiddenagenda/delphi/__init__.py
+++ hiddenagenda/delphi/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:587f2798bdb25d74859cffca0e5bb8dfac178e2e6b17e29e5d791b15ffd5c96b
-size 38851
+oid sha256:794f9f4ca460dea73c840bed882ae7a4aba70295390a2dd74bc003f61fd1083d
+size 38908
