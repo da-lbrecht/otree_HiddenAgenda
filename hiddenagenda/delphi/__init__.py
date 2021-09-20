330775e57da29ca679d0eae6f56d3860a5149300
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-20 17:13:38 +0200

    Test all pages.

--- hiddenagenda/delphi/__init__.py
+++ hiddenagenda/delphi/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82803b27ff8bd44ae2a578895cbd07498f02a29638686514718abe291c865b4f
+oid sha256:84c43908c03a6e72cf0cba2595b55df5bec0fed098fffdcd9f1dc713f6dbc951
 size 44395
