707c84ff52a2336782cf7514f40e8583cee3197a
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-20 08:21:56 +0200

    Change currency from points to EUR.

--- hiddenagenda/settings.py
+++ hiddenagenda/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea4e2d3f35ebbc443b6fc26d3aa75c4b5cbac5c9ca2d087a0e19e6dea8a09c2c
-size 1371
+oid sha256:45d8990c9ce8941025b22a0e18cc5ebfc3e74436905067c733a7e9d389e0ddaf
+size 1372
