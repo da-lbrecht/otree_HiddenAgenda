2a5f4d0ffa8ad14ff876b8c93ee15e2981baf813
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-20 15:38:38 +0200

    Create barebone for honesty likert scales

--- hiddenagenda/delphi/__init__.py
+++ hiddenagenda/delphi/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:835b0a5c9a335d0dc446f928453d8ee7b7bda0a8bddb7bc48ca4410558241b5e
-size 44970
+oid sha256:f02cc308c9d16896fac99fc3092b1f190facd28be1f29ff805a730d2175af647
+size 44566
