f3e7eb706f288a295d78a80c6f58301188de707c
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-20 11:56:49 +0200

    Fix task intro from git history.

--- hiddenagenda/delphi/__init__.py
+++ hiddenagenda/delphi/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a686d597f5f6d19882d45bddce48b6c729750f73d8353b54d8d7eefd51fd3f8
-size 40953
+oid sha256:ab0cc3822588a6f8ad168a7ec3c47fc962fddb2d5c7df3cea9d8cb5c47274a2e
+size 40723
