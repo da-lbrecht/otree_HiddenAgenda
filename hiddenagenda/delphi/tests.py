d934de28e58d20abd88d25abb262c831f3f2d1fe
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-21 10:20:09 +0200

    Set up testing bot barebone.

---
+++ hiddenagenda/delphi/tests.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ccf13ede11562ef200b32a0149144ef3d1acdf3009f662c06f14125344d451c
+size 512
--- hiddenagenda/settings.py
+++ hiddenagenda/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49676698adb69cf4a2c6475b78b50bc5aec3aede37874f2001ad10b1ea81f782
-size 1401
+oid sha256:f925c42f500185624342f489c58a7f22262cdca683d0b2ce2cc283e9f32fa818
+size 1584
