b47fa98eeed2a6e81e15fe21d905de5694246fa7
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-21 12:07:27 +0200

    Testing entire experiment with 2 rounds.

--- hiddenagenda/settings.py
+++ hiddenagenda/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a59de55543c3ed961a2eb8f74fff4542acfcc286c4c8f1c208817d9dd1c1701b
-size 1587
+oid sha256:5af85a6f10b4d9d361de061f6f3aa7eee2ea6708fee37e1687dda037b4d80ad4
+size 1585
