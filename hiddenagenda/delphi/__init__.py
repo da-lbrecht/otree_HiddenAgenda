019ae437ebff51079782bede934368a4423e455d
dalbrechtNL <d.albrecht@maastrichtuniversity.nl>
2021-09-22 09:07:56 +0200

    Display larger input fields for open answer questions in Questionnaire.html

--- hiddenagenda/delphi/__init__.py
+++ hiddenagenda/delphi/__init__.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19a1b25081a63ddba2c1ec2ded1d330f6cb056ec1e7418dce3422dbf16add98a
-size 44401
+oid sha256:84c43908c03a6e72cf0cba2595b55df5bec0fed098fffdcd9f1dc713f6dbc951
+size 44395
